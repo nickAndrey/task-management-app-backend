f533c025d6eeaa26c378710f9d430a50c5d54638
Andrew Husiev <andrew@magnificode.net>
2024-08-17 20:28:10 -0700

    add minor fixes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
+import { configDotenv } from 'dotenv';
 import express from 'express';
 
+configDotenv();
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
