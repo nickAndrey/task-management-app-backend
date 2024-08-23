c99d75c50f2210dcc8b2cd9fe76eb27f337eff20
Andrew Husiev <andrew@magnificode.net>
2024-08-22 22:07:13 -0700

    - add user delete method - add get user by id method - add get all users method

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import createUser from '../controllers/users/createUser';
+import deleteUser from '../controllers/users/deleteUser';
+import getUser from '../controllers/users/getUser';
+import getUsers from '../controllers/users/getUsers';
 import updateUser from '../controllers/users/updateUser';
 import validateUserCreateDto from '../validators/validateUserCreateDto';
 import validateUserUpdateDto from '../validators/validateUserUpdateDto';
 
 const router = Router();
+
+router.get('/', getUsers);
+
+router.get('/:id', getUser);
 
 router.post('/create', (req, res) => {
   const error = validateUserCreateDto(req.body);
@@ -25,5 +32,7 @@ router.put('/update', (req, res) => {
 
   updateUser(req, res);
 });
+
+router.delete('/:id', deleteUser);
 
 export default router;
